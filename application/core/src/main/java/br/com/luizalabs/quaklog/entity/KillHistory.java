8005ad2173085558e68a4d21f3de14f3493e948c
Guilherme Biff Zarelli <gbzarelli@gmail.com>
2019-12-10 18:04:51 -0300

    feat(core): KillHistory
    - Removido acesso recursivo aos objetos

--- application/core/src/main/java/br/com/luizalabs/quaklog/entity/KillHistory.java
+++ application/core/src/main/java/br/com/luizalabs/quaklog/entity/KillHistory.java
@@ -12,10 +12,10 @@ public class KillHistory {
 
     private final GameTime gameTime;
     private final KillMode killMode;
-    private final Player player;
+    private final Integer player;
     private final Mod mod;
 
-    private KillHistory(GameTime gameTime, KillMode killMode, Player player, Mod mod) {
+    private KillHistory(GameTime gameTime, KillMode killMode, Integer player, Mod mod) {
         this.gameTime = gameTime;
         this.killMode = killMode;
         this.player = player;
@@ -23,10 +23,10 @@ public class KillHistory {
     }
 
     static KillHistory killed(GameTime gameTime, Player player, Mod mod) {
-        return new KillHistory(gameTime, KillMode.KILL, player, mod);
+        return new KillHistory(gameTime, KillMode.KILL, player.getId(), mod);
     }
 
     static KillHistory deadBy(GameTime gameTime, Player player, Mod mod) {
-        return new KillHistory(gameTime, KillMode.DEAD, player, mod);
+        return new KillHistory(gameTime, KillMode.DEAD, player.getId(), mod);
     }
 }
